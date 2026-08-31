fb4850bf00399a70c9f141a3603eb2f10f2ecfe3
David Mbozi <davidmbozi4@gmail.com>
2026-09-01 00:05:48 +0200

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const downloadButton = document.getElementById("downloadButton");
 
 
 // API address
-const API_URL = "https://strength-bean-barbie-idle.trycloudflare.com";
+const API_URL = "https://florida-compatible-frozen-cbs.trycloudflare.com";
 
 
 // Store selected file
