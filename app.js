515b8acdc44fc99328a6b1ab6c12baff53723018
David Mbozi <davidmbozi4@gmail.com>
2026-08-31 23:50:58 +0200

    Update Cloudflare API URL

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const downloadButton = document.getElementById("downloadButton");
 
 
 // API address
-const API_URL = "https://leaf-arrow-memorabilia-exclude.trycloudflare.com";
+const API_URL = "https://leaf-arrow-memorabilia-exclude.trycloudflare.com/separate";
 
 
 // Store selected file
